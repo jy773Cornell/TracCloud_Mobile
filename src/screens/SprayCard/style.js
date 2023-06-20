5bdf057f0e599660574eb4544066720b28873695
Jinhong Yu <jy773@cornell.edu>
2023-06-20 09:02:46 -0400

    spray card table

---
+++ src/screens/SprayCard/style.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff'},
+    head: {height: 40, backgroundColor: '#808B97'},
+    text: {margin: 6},
+    row: {flexDirection: 'row', backgroundColor: '#FFF1C1'},
+    btn: {width: 58, height: 18, backgroundColor: '#78B7BB', borderRadius: 2},
+    btnText: {textAlign: 'center', color: '#fff'}
+});
