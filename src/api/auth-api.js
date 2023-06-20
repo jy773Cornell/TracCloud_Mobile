6afe52a4345b799a1033e83e21ed211d5c962ca6
Jinhong Yu <jy773@cornell.edu>
2023-06-19 20:16:34 -0400

    update login

--- src/api/auth-api.js
+++ src/api/auth-api.js
@@ -1,14 +1,43 @@
-export const loginUser = async ({username, password}) => {
+export const getCSRFToken = async () => {
     const requestOptions = {
-        method: "POST",
-        headers: {
+        method: "GET", headers: {
+            'Accept': 'application/json', "Content-Type": "application/json",
+        },
+    };
+
+    try {
+        // const response = await fetch("http://127.0.0.1:8000/api/csrf/get/", requestOptions)
+        const response = await fetch("https://tracloud.azurewebsites.net/api/csrf/get/", requestOptions)
+        if (response.ok) {
+            return (response.json());
+        } else {
+            const error = await response.json();
+            console.log(error);
+            return {
+                error: error.message || 'Something went wrong',
+            }
+        }
+    } catch (error) {
+        return {
+            error: error.message,
+        }
+    }
+}
+
+export const loginUser = async ({username, password}) => {
+    const csrfResponse = await getCSRFToken();
+    if (!csrfResponse.csrfToken) {
+        return ({error: "csrfToken not found"})
+    }
+
+    const csrfToken = csrfResponse.csrfToken;
+    const requestOptions = {
+        method: "POST", headers: {
             'Accept': 'application/json',
             "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            username: username,
-            password: password,
-            remember: true,
+            'X-CSRFToken': csrfToken,
+        }, body: JSON.stringify({
+            username: username, password: password, remember: true,
         }),
     };
 
@@ -19,9 +48,10 @@ export const loginUser = async ({username, password}) => {
             return (response.json());
         } else {
             const error = await response.json();
-            if (response.statusText === "Not Found") {
+            console.log(error);
+            if (response.status === 404) {
                 return {error: "username",}
-            } else if (response.statusText === "Forbidden") {
+            } else if (response.status === 403) {
                 return {error: "password",}
             }
             return {
